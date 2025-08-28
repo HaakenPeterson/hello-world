ad5c146ddf8c4e142caa5ea885516939620509cc
Haaken Peterson <hpeterson16@huskers.unl.edu>
2025-08-28 10:45:58 -0500

    hello.c

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Haaken Peterson
+ *         Hpeterson16@Huskers.unl.edu
+ * Date: 8/28/25
+ *
+ * My Hack Hello.c File
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Haaken Peterson!\n");
+    printf("Computer Engineering!\n");
+
+
+
+    return 0;
+}
